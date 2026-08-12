d32f6e66c315f20c7742c478d03bd06e40be4611
Deeep10 <deep@sprybit.com>
2026-08-12 17:29:27 +0530

    Add 1st Commit

--- NotesApi/Models/NoteItem.cs
+++ NotesApi/Models/NoteItem.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
+        public string? highlightLine { get; set; }
         public bool ReadOrNot { get; set; }
     }
 }
