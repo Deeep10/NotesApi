45d5adec277854d1d8509240324c2665793c4115
Deeep10 <deep@sprybit.com>
2026-08-12 17:10:53 +0530

    Add project files.

---
+++ NotesApi/Controllers/NotesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesApi.Models;
+
+namespace NotesApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private static readonly List<NoteItem> Notes = new()
+    {
+        new NoteItem
+        {
+            Id = 1,
+            Title = "1st Note",
+            Content = "Bikes",
+            ReadOrNot = false
+        },
+        new NoteItem
+        {
+            Id = 2,
+            Title = "2nd Notes",
+            Content = "Cars",
+            ReadOrNot = false
+        }
+    };
+
+        [HttpGet]
+        public IActionResult GetNotes()
+        {
+            return Ok(Notes);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNote(NoteItem note)
+        {
+            note.Id = Notes.Count + 1;
+
+            Notes.Add(note);
+
+            return CreatedAtAction(
+                nameof(note),
+                new { id = note.Id },
+                note);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateNote(int id, NoteItem updatedNote)
+        {
+            var note = Notes.FirstOrDefault(x => x.Id == id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            note.Title = updatedNote.Title;
+            note.Content = updatedNote.Content;
+            note.ReadOrNot = updatedNote.ReadOrNot;
+
+            return Ok(note);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNote(int id)
+        {
+            var note = Notes.FirstOrDefault(x => x.Id == id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            Notes.Remove(note);
+
+            return NoContent();
+        }
+    }
+}
---
+++ NotesApi/Models/NoteItem.cs
@@ -0,0 +1,10 @@
+﻿namespace NotesApi.Models
+{
+    public class NoteItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public bool ReadOrNot { get; set; }
+    }
+}
